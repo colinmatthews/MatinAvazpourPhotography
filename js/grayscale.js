bf5a9bbb4fc8f10958c5c1fd7e8e04dcc728cfde
avazpour <matinavazpour@Arshias-MacBook-Pro.local>
2016-12-21 17:35:50 -0500

    .

--- js/grayscale.js
+++ js/grayscale.js
@@ -48,7 +48,7 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(42.3149, 83.0364), // Windsor, Ontario
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
